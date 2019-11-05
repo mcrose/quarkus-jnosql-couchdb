7ab1fd8e38b001313abd868570d1d02a249653b6
Roberto Gamarra [rgamarra@ingenia.io] <Roberto Gamarra [rgamarra@ingenia.io]@ikki>
2019-11-05 16:44:29 -0300

    basic quarkus files

---
+++ src/test/java/py/com/icarusdb/quarkus/jnosql/couchdb/NativeGreetingResourceIT.java
@@ -0,0 +1,9 @@
+package py.com.icarusdb.quarkus.jnosql.couchdb;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeGreetingResourceIT extends GreetingResourceTest {
+
+    // Execute the same tests but in native mode.
+}
